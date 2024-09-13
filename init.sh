8aff87285bd14bb490485f970157651f15efeac3
Logan <logand007@outlook.com>
2024-09-13 15:27:24 -0500

    changed where the scripts are stored

--- init.sh
+++ init.sh
@@ -9,10 +9,15 @@ fi
 mkdir $HOME/.config/screenshot-utils
 #create the file the scripts go into
 mkdir $HOME/.scripts
+mkdir $HOME/.scripts/screenshot-utils
 
+#setup config files
 echo "FALSE" > $HOME/.config/screenshot-utils/copy-path
+mkdir $HOME/Pictures/Screenshots
+echo $HOME/Pictures/Screenshots > $HOME/.config/screenshot-utils/copy-path
 
-mkdir $HOME/Photos/Screenshots
-echo $HOME/Photos/Screenshots > $HOME/.config/screenshot-utils/copy-path
+mv screenshot selective_screenshot window_screenshot $HOME/.scripts/screenshot-utils/
 
-mv screenshot selective_screenshot window_screenshot $HOME/.scripts
+echo "Installation finished."
+echo "To add the scripts to your path run"
+echo "export PATH=$HOME/.scripts/screenshot-utils:$PATH"
