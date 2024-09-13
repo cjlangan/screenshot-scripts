6c413967eb564006d4baea9ac028ba1ff6ae37f4
Logan <logand007@outlook.com>
2024-09-13 15:35:52 -0500

    init.sh now removes it's self and fixed bug where screenshot-path was not created

--- init.sh
+++ init.sh
@@ -14,10 +14,14 @@ mkdir $HOME/.scripts/screenshot-utils
 #setup config files
 echo "FALSE" > $HOME/.config/screenshot-utils/copy-path
 mkdir $HOME/Pictures/Screenshots
-echo $HOME/Pictures/Screenshots > $HOME/.config/screenshot-utils/copy-path
+echo $HOME/Pictures/Screenshots > $HOME/.config/screenshot-utils/screenshot-path
 
 mv screenshot selective_screenshot window_screenshot $HOME/.scripts/screenshot-utils/
 
+echo ""
 echo "Installation finished."
+echo "You can now remove this folder."
 echo "To add the scripts to your path run"
-echo "export PATH=$HOME/.scripts/screenshot-utils:$PATH"
+echo "export PATH=\$HOME/.scripts/screenshot-utils:\$PATH"
+
+rm init.sh
