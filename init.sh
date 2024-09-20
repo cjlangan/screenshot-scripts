f75ab2b5d25e849a710e905d6cafd59e0babba74
cjlangan <connorjameslangan@gmail.com>
2024-09-20 14:56:35 -0500

    removed config file needs from installation

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#setup the config files
-mkdir $HOME/.config/gnome-screenshot-utils
-mkdir $HOME/Pictures/Screenshots
-echo $HOME/Pictures/Screenshots > $HOME/.config/screenshot-utils/screenshot-path
-
-cp gscreen /usr/local/bin/gscreen
-
+chmod +x gscreen.sh
+cp gscreen.sh /usr/local/bin/gscreen
 echo "Installation finished."
