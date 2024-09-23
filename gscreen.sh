c10d8d2d2d4d772705bb0832522a7520b179c9d0
cjlangan <connorjameslangan@gmail.com>
2024-09-22 23:01:57 -0500

    Added support for non-standard environments, such as gtile's command via windows-key + /

--- gscreen.sh
+++ gscreen.sh
@@ -37,19 +37,19 @@ filename=$(date +'%Y-%m-%d-%H.%M.%S').png
 filepath="$HOME/Pictures/Screenshots"
 
 if [ "$before" == "true" ]; then
-    echo $filepath/$filename | wl-copy
+    echo $filepath/$filename | dbus-launch wl-copy
 fi
 
 # Take screenshot and copy to clipboard
 if [ "$entire" == "true" ]; then # take regular screenshot
-    gnome-screenshot -f "$filepath/$filename" | wl-copy
+    gnome-screenshot -f "$filepath/$filename" | dbus-launch wl-copy
 elif [ "$window" == "true" ]; then # take screenshot of window
-    gnome-screenshot -w -f "$filepath/$filename" | wl-copy
+    gnome-screenshot -w -f "$filepath/$filename" | dbus-launch wl-copy
 else # take selective screenshot
-    gnome-screenshot -a -f "$filepath/$filename" | wl-copy
+    gnome-screenshot -a -f "$filepath/$filename" | dbus-launch wl-copy
 fi
 
 if [ "$before" == "false" ]; then
-    echo $filepath/$filename | wl-copy
+    echo $filepath/$filename | dbus-launch wl-copy
 fi
 
