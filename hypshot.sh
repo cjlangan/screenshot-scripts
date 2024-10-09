9805961abde48507f14d91d94d1eeb5d0d50180a
cjlangan <connorjameslangan@gmail.com>
2024-10-09 12:26:55 -0500

    Added screen shot tool utilizing grim and slurp

---
+++ hypshot.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Function to show help
+function show_help
+{
+    echo "Usage: gscreen [-e] [-w] [-b]"
+    echo "  -e      take screenshot of entire screen"
+    echo "  -w      take screenshot of current window"
+    echo "  -b      copy file path to clipboard before the screenshot"
+    echo "with no flags, it will be a selective screenshot and the"
+    echo "file path will be copied to your clipboard after"
+}
+
+entire=false
+window=false
+before=false
+
+while getopts "ewb" opt; do
+    case ${opt} in
+        e )
+            entire=true
+            ;;
+        w )
+            window=true
+            ;;
+        b )
+            before=true
+            ;;
+        \? )
+            show_help
+            exit 1
+            ;;
+    esac
+done
+
+filename=$(date +'%Y-%m-%d-%H.%M.%S').png
+filepath="$HOME/Pictures/Screenshots"
+
+if [ "$before" == "true" ]; then
+    echo $filepath/$filename | wl-copy
+fi
+
+# Take screenshot and copy to clipboard
+if [ "$entire" == "true" ]; then # take regular screenshot
+    grim - | tee $filepath/$filename | wl-copy
+elif [ "$window" == "true" ]; then # take screenshot of window
+    grim -g "$(slurp -w)" - | tee $filepath/$filename | wl-copy
+else # take selective screenshot
+    grim -g "$(slurp)" - | tee $filepath/$filename | wl-copy
+fi
+
+if [ "$before" == "false" ]; then
+    echo $filepath/$filename | wl-copy
+fi
+
